0a83f489eda6a92f1b3ddb458ca3b11348ae29e9
jonJJ2016 <jonmartirosyan1988@gmail.com>
2022-09-18 04:44:26 +0800

    added post patch get by id delete routes

--- router/project.router.js
+++ router/project.router.js
@@ -1,8 +1,13 @@
 const express = require('express')
-const { findQuery, CRUD } = require('../../reqBobby')
+const { findQuery, CRUD, getQuery } = require('../../reqBobby')
 const Project = require('../Models/Projects')
 
 const router = express.Router()
 
 router.get('/', findQuery(Project), CRUD(Project).find)
+router.post('/', CRUD(Project).create)
+router.get('/:id', getQuery(Project), CRUD(Project).get)
+router.patch('/:id', CRUD(Project).patch)
+router.delete('/:id', CRUD(Project).remove)
+
 module.exports = router
