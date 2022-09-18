7439c809b148e73524519bf37e59f6462f145ab4
jonJJ2016 <jonmartirosyan1988@gmail.com>
2022-09-18 23:09:29 +0800

    added asynch handler

--- router/project.router.js
+++ router/project.router.js
@@ -1,10 +1,16 @@
 const express = require('express')
-const { findQuery, CRUD, getQuery } = require('bobby_query')
+const { findQuery, CRUD, getQuery, asyncHandler } = require('../../reqBobby')
 const Project = require('../Models/Projects')
 
 const router = express.Router()
 
-router.get('/', findQuery(Project), CRUD(Project).find)
+router.get(
+  '/',
+  findQuery(Project),
+  asyncHandler(async (req, res) => {
+    res.status(200).json({ ...req.Bobby })
+  }),
+)
 router.post('/', CRUD(Project).create)
 router.get('/:id', getQuery(Project), CRUD(Project).get)
 router.patch('/:id', CRUD(Project).patch)
