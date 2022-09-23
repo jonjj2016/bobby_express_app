e796d3b17c5cf87288c7b30286d3aacaece43798
jonJJ2016 <jonmartirosyan1988@gmail.com>
2022-09-24 01:47:42 +0800

    aerfvaer

--- db.js
+++ db.js
@@ -1,6 +1,6 @@
 const mongoose = require('mongoose')
 
-mongoose.connect(process.env.REACT_APP_MONGO_DB, {
+mongoose.connect(process.env.MONGO_DB, {
   useNewUrlParser: true,
   useUnifiedTopology: true,
 })
