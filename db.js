166fa9e44c2e3bc3b4f412b214870b5b6fa0073a
jonJJ2016 <jonmartirosyan1988@gmail.com>
2022-09-24 01:44:41 +0800

    aerfvaer

--- db.js
+++ db.js
@@ -1,9 +1,6 @@
 const mongoose = require('mongoose')
 
-mongoose.connect(
-  'mongodb+srv://jonmartirosyan1988:5547301988jj+-@cluster0.cdjg1.mongodb.net/?retryWrites=true&w=majority',
-  {
-    useNewUrlParser: true,
-    useUnifiedTopology: true,
-  },
-)
+mongoose.connect(process.env.REACT_APP_MONGO_DB, {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+})
